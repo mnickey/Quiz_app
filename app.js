a86ad8f56883cdffa79b29354cfb872cc9311468
Michael Nickey <mnickey@gmail.com>
2016-10-19 21:17:58 -0700

    adding questions to state object

--- app.js
+++ app.js
@@ -4,9 +4,21 @@
 
 // State object
 var state = {
-    questions: {
-        text: "lorem",
-        answers: ["a", "b", "c", "d"],
-        correctAnswer: "a"
-    }
+    questions: [{
+        text: "What is the largest prime number listed?",
+        answers: ["31", "27", "29", "23"],
+        correctAnswer: "29"
+    },
+    {
+        text: "How many letters are in the Hawaiian alphabet??",
+        answers: ["26", "19", "12/13", "17"],
+        correctAnswer: "12/13"
+    }],
+    score: 0,
+    questionIndex: 0
 };
+
+// Document ready
+$(document).ready(function () {
+    console.log("ready");
+});
