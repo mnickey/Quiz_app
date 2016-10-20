802eedf3dc8ba892348626f2a0d4db26f1802665
Michael Nickey <mnickey@gmail.com>
2016-10-19 19:57:51 -0700

    added index and app.js files

---
+++ app.js
@@ -0,0 +1,12 @@
+/**
+ * Created by MNickey on 10/19/16.
+ */
+
+// State object
+var state = {
+    questions: {
+        text: "lorem",
+        answers: ["a", "b", "c", "d"],
+        correctAnswer: "a"
+    }
+};
