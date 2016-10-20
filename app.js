eb5e460fa7b947ba4365b33e555e90de53f8f56e
Michael Nickey <mnickey@gmail.com>
2016-10-19 22:04:45 -0700

    adding questions to state object

--- app.js
+++ app.js
@@ -4,16 +4,37 @@
 
 // State object
 var state = {
-    questions: [{
-        text: "What is the largest prime number listed?",
-        answers: ["31", "27", "29", "23"],
-        correctAnswer: "29"
-    },
-    {
-        text: "How many letters are in the Hawaiian alphabet??",
-        answers: ["26", "19", "12/13", "17"],
-        correctAnswer: "12/13"
-    }],
+    questions: [
+        {
+            text: "The string of flowers sewn together and tied at the ends to form a circle is called a",
+            answers: ["lei", "rosary", "poi", "hula", "crea"],
+            correctAnswer: "lei"
+        },
+        {
+            text: "How many letters are in the Hawaiian alphabet??",
+            answers: ["26", "19", "12/13", "17", "21"],
+            correctAnswer: "12/13"
+        },
+        {
+            text: "The ship that was sunk on the Pearl Harbor attack is called:",
+            answers: ["California", "Texas", "Arizona", "Deleware", "Maine"],
+            correctAnswer: "Arizona"
+        },
+        {
+            text: "Hawaii is the only U.S. state that grows...",
+            answers: ["marijuana", "coffee", "tea", "oranges", "grapefruit"],
+            correctAnswer: "coffee"
+        },
+        {
+            text: "How many cans of Spam do Hawaiians eat each year?",
+            answers: ["400,000", "1,000,000", "750,000", "6,000,000", "2,000,000"],
+            correctAnswer: "6,000,000"
+        },
+        {
+            text: "Hawaii is the only US state with a ...",
+            answers: ["tropical rain forest", "olympic surf team", "volcano", "king", "spam plant"],
+            correctAnswer: "tropical rain forest"
+        }],
     score: 0,
     questionIndex: 0
 };
