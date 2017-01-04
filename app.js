f980abec4c44444583b908ed0b3f42041e79b936
Michael Nickey <mnickey@gmail.com>
2017-01-03 18:28:04 -0800

    scoring working, added reset function to handle state object resets

--- app.js
+++ app.js
@@ -4,52 +4,25 @@
 
 // state objects
 let divs = document.querySelectorAll(`.container div`);
-// let button = document.querySelectorAll('btn');
-
 let state = {
     count: 0,
-    // Set Correct Answers
-    answers : ["b", "a", "d", "b", "d"],
-    score : 0,
-    total : 5
+    answers: ["b", "a", "d", "b", "d"], // Set Correct Answers
+    score: 0,
+    total: 5
 };
 
-function submitAnswers() {
-    let total = 5;
-    let score = 0;
+// Validation
+// for (let valid = 1; valid <= total; valid++) {
+//     if (eval('q' + valid) == null || eval('q' + valid) == '') {
+//         alert('You missed question ' + valid);
+//         return false;
+//     }
+// }
 
-    // Get User Input
-    // let q1 = document.forms["quizForm"]["q1"].value;
-    // let q2 = document.forms["quizForm"]["q2"].value;
-    // let q3 = document.forms["quizForm"]["q3"].value;
-    // let q4 = document.forms["quizForm"]["q4"].value;
-    // let q5 = document.forms["quizForm"]["q5"].value;
-
-    // Validation
-
-    // for (let valid = 1; valid <= total; valid++) {
-    //     if (eval('q' + valid) == null || eval('q' + valid) == '') {
-    //         alert('You missed question ' + valid);
-    //         return false;
-    //     }
-    // }
-
-    // Set Correct Answers
-    // let answers = ["b", "a", "d", "b", "d"];
-
-    // Check Answers
-    // for (let check = 1; check <= total; check++) {
-    //     if (eval('q' + check) == answers[check - 1]) {
-    //         score++;
-    //     }
-    // }
-
-// Display Results
-
-    let results = document.getElementById('score');
-    results.innerHTML = '<h3>You scored <span>' + score + '</span> out of <span>' + total + '</span></h3>';
-    alert('You scored ' + score + ' out of ' + total);
-    return false;
+// Display Score
+function displayScore(state) {
+    let results = document.getElementById('q6');
+    results.innerHTML = '<h3>You scored <span>' + state.score + '</span> out of <span>' + state.total + '</span></h3>';
 }
 
 function checkAnswer(state) {
@@ -67,6 +40,13 @@ function checkAnswer(state) {
         }
     }
     console.log('score: ' + state.score);
+    return state.score;
+}
+
+function resetQuiz(state) {
+    state.count = 0;
+    state.score = 0;
+    return state;
 }
 
 function changeButtonText(state) {
@@ -78,8 +58,8 @@ function changeButtonText(state) {
     }
     else if (state.count > 5) {
         $('.submit-btn').val('retake quiz');
-        state.count = 0;
-        state.score = 0;
+        displayScore(state);
+        resetQuiz(state);
     }
 }
 
@@ -88,15 +68,9 @@ $('.submit-btn').click(function (event) {
     event.preventDefault();
 
     divs.forEach(ea => ea.classList.add('hidden'));
-    state.count = state.count < divs.length - 1 ? state.count + 1: 0;
+    state.count = state.count < divs.length - 1 ? state.count + 1 : 0;
     divs[state.count].classList.remove('hidden');
-    changeButtonText(state);
+
     checkAnswer(state);
+    changeButtonText(state);
 });
-
-// $('.btn').addEventListener('click', function (state) {
-    // divs.forEach(ea => ea.classList.add('hidden'));
-    // state.count = state.count < divs.length - 1 ? state.count + 1: 0;
-    // divs[state.count].classList.remove('hidden');
-// });
-
